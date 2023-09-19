a6059b73f6a932819254f35031f06b261c462753
Murat Küçük <muratkucuk@std.iyte.edu.tr>
2023-09-19 15:58:07 +0300

    Version 4

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,6 +5,58 @@ let score = JSON.parse(localStorage.getItem("score")) || {
 };
 updateScoreElements();
 
+
+document.querySelector('.rock_button').addEventListener('click', () => {
+  playGame('rock');
+});
+document.querySelector('.paper_button').addEventListener('click', () => {
+  playGame('paper');
+});
+document.querySelector('.scissors_button').addEventListener('click', () => {
+  playGame('scissors');
+});
+document.querySelector('.js_reset_button').addEventListener('click', () => {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElements();
+});
+document.querySelector('.js_auto_play_button').addEventListener('click', () => {
+  autoPlay();
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if(event.key === 'r'){
+    playGame('rock');
+  }
+  else if(event.key === 'p'){
+    playGame('paper');
+  }
+  else if(event.key === 's'){
+    playGame('scissors');
+  }
+});
+
+
+let intervalId;
+let isAutoPlay = false;
+function autoPlay(){
+  
+  if(!isAutoPlay){
+    document.querySelector('.auto_play_button').innerHTML = 'Stop Playing';
+    intervalId = setInterval(() => {
+      playGame(pickComputerMove());
+    }, 1000);
+    isAutoPlay = true;
+  }
+  else{
+    clearInterval(intervalId);
+    document.querySelector('.auto_play_button').innerHTML = 'Auto Play';
+    isAutoPlay = false;
+  }
+}
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
   let result = "";
